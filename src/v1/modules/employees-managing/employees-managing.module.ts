5349080cc6bee3bb31e57d37a2d6875dcf774a63
Diego Marquina <diegomh210@gmail.com>
2024-07-19 15:39:09 -0500

    Refactor employee module

--- src/v1/modules/employees-managing/employees-managing.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmployeesManagingService } from './services/employees-managing.service';
-import { EmployeesManagingController } from './controllers/employees-managing.controller';
-
-@Module({
-  controllers: [EmployeesManagingController],
-  providers: [EmployeesManagingService],
-})
-export class EmployeesManagingModule {}
--- src/v1/modules/employees/employees.module.ts
+++ src/v1/modules/employees/employees.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmployeesService } from './services/employees.service';
 import { EmployeesController } from './controllers/employees.controller';
+import { EmployeesManagingController } from './controllers/employees-managing.controller';
+import { EmployeesManagingService } from './services/employees-managing.service';
 
 @Module({
-  controllers: [EmployeesController],
-  providers: [EmployeesService],
+  controllers: [EmployeesController, EmployeesManagingController],
+  providers: [EmployeesService, EmployeesManagingService],
 })
 export class EmployeesModule {}
